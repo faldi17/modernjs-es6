d6bdfca8830de584ed8dbe31bd8b90d7d1a73189
Faldiansyah <faldiansyahk@gmail.com>
2025-02-25 00:08:47 +0700

    arrow function

--- arrow-func.js
+++ arrow-func.js
@@ -8,7 +8,16 @@
 rectangle = (x, y) => x * y; // ini arrow function
 
 rec1 = rectangle(5, 3);
-console.log(rec1);
+console.log(`Hasil dari 5 x 3 = ${rec1}`);
 
 rec2 = rectangle(10, 7);
-console.log(rec2);
+console.log(`Hasil dari 10 x 7 = ${rec2}`);
+
+rec3 = rectangle(30, 19);
+console.log(`Hasil dari 30 x 19 = ${rec3}`);
+
+rec4 = rectangle(50, 45);
+console.log(`Hasil dari 50 x 45 = ${rec4}`);
+
+rec5 = rectangle(150, 120);
+console.log(`Hasil dari 150 x 120 = ${rec5}`);
