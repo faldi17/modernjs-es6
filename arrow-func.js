e24774f5b6e45192fb0c917132b2ff00ca39b09e
Faldiansyah <faldiansyahk@gmail.com>
2025-02-25 00:02:31 +0700

    arrow function

---
+++ arrow-func.js
@@ -0,0 +1,14 @@
+/*
+    ini cara lama nih
+    function rectangle(x) {
+        return x * x;
+    }
+*/
+
+rectangle = (x, y) => x * y; // ini arrow function
+
+rec1 = rectangle(5, 3);
+console.log(rec1);
+
+rec2 = rectangle(10, 7);
+console.log(rec2);
